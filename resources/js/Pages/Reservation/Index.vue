c7b1c3bf8cfa6ebdecb11e3b8c9289152db40ba7
Gacs Péter <g.silver.014@gmail.com>
2024-04-11 15:48:13 +0200

    add Calendar

---
+++ resources/js/Pages/Reservation/Index.vue
@@ -0,0 +1,35 @@
+
+<template>
+    <div class>
+        <calendar
+            @updateData =  "handleUpdate"
+            :year="2024"
+            :month="3">
+        </calendar>
+        {{ selectedDay }}
+    </div>
+</template>
+
+<script>
+import Calendar from "@/Components/Calendar.vue";
+
+export default {
+    name: "index",
+    components: {Calendar},
+
+    data() {
+        return {
+            selectedDay: 0,
+        }
+    },
+    methods: {
+        handleUpdate(data) {
+            this.selectedDay = data;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
